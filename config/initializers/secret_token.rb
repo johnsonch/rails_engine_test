c45a61b3c34ef52c685e6be78a1931cf1973f29a
Puch user on mural.local <punch@mural.local>
2011-05-10 14:12:03 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EngineDemo::Application.config.secret_token = 'b022a9f109d07c24ca05188da5511efb68d92b665919224332a73f8bb4c400b76d9e1f0717c1e10a95e557c00e4c0a6e4854f5ce09e7d24d251e660410b7a0f4'
